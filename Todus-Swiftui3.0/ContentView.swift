c7c0ac9717fb4660fa2b54155668cf5e9d94903c
Asiel Cabrera <cabrerasiel@gmail.com>
2021-08-22 03:14:28 -0400

    Initial Commit

---
+++ Todus-Swiftui3.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todus-Swiftui3.0
+//
+//  Created by Asiel Cabrera Gonzalez on 22/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todus-Swiftui3.0/Todus_Swiftui3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Todus_Swiftui3_0App.swift
+//  Todus-Swiftui3.0
+//
+//  Created by Asiel Cabrera Gonzalez on 22/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Todus_Swiftui3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
